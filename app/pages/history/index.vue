1ab77e73b2118e19a1dc7ccab960a04985aea77a
ririo08 <7778943+ririo08@users.noreply.github.com>
2025-05-30 01:14:32 +0900

    feat: 活動履歴 置き換え

--- app/pages/history/index.vue
+++ app/pages/history/index.vue
@@ -1,115 +1,84 @@
-<script>
-export default {
-  data() {
-    return { list: [], date: 0, year: 0 }
-  },
-  async fetch() {
-    // const res = await this.$axios.$get(
-    //   'https://ririo.dev/db/history-index.json'
-    // )
-    // this.list = res.reverse()
-  },
-  mounted() {
-    const a = Date.parse('2013/02/03')
-    const b = new Date()
-    let c = (b - a) / 1000 / 60 / 60 / 24
-    const d = Math.round(c / 365) * 1 + 1
-    c = Math.floor(c)
-    this.date = c
-    this.year = d
-  },
+<script setup lang="ts">
+type History = {
+  Id: string
+  Date: string
+  Detail: string
+  Link: string
+  IsYear: string
 }
+
+const { data: list } = await useFetch<History[]>('https://ririo.dev/db/history-index.json', { default: () => [] })
+
+const sortedList = computed(() =>
+  list.value.slice().reverse(),
+)
+
+const date = ref(0)
+const year = ref(0)
+
+// TODO: 計算方法作り直す
+onMounted(() => {
+  const a = Date.parse('2013/02/03')
+  const b = new Date()
+  let c = (b.getTime() - a) / 1000 / 60 / 60 / 24
+  const d = Math.round(c / 365) * 1 + 1
+  c = Math.floor(c)
+  date.value = c
+  year.value = d
+})
 </script>
 
 <template>
   <div>
-    <div class="globalBox">
-      <h1 class="globalBox-title">
-        HISTORY
-      </h1>
-      <p class="history-details_p">
-        活動の記録です。現在{{ date }}日目（{{ year }}年目）！
-      </p>
-      <p class="history-details_p">
-        YouTube Liveの記録はこちら →
-        <NuxtLink to="/history/live">
-          YouTube Live HISTORY
-        </NuxtLink>
-      </p>
-      <p class="history-details_p">
-        Twitchの過去の記録はこちら →
-        <NuxtLink to="/history/twitch">
-          Twitch Ririo08's HISTORY
-        </NuxtLink>
-      </p>
-    </div>
-    <div class="historyList">
-      <ul class="historyList-ul">
-        <span
-          v-for="item in list"
-          :key="item.Id"
+    <h2 class="text-2xl text-center font-bold border-b-2 border-primary-500 pb-2 mb-4">
+      HISTORY
+    </h2>
+    <p class="mt-4">
+      活動の記録です。現在{{ date }}日目（{{ year }}年目）！
+    </p>
+    <p class="mt-4">
+      YouTube Liveの記録はこちら →
+      <NuxtLink
+        to="/history/live"
+        class="text-info-500 hover:underline"
+      >
+        YouTube Live HISTORY
+      </NuxtLink>
+    </p>
+    <p class="mt-4">
+      Twitchの過去の記録はこちら →
+      <NuxtLink
+        to="/history/twitch"
+        class="text-info-500 hover:underline"
+      >
+        Twitch Ririo08's HISTORY
+      </NuxtLink>
+    </p>
+    <ul>
+      <template
+        v-for="item in sortedList"
+        :key="item.Id"
+      >
+        <li
+          :class="[
+            'font-bold md:flex gap-x-4',
+            item.IsYear === '1' ? 'text-red-600 mt-10' : 'mt-2',
+          ]"
         >
-          <li
-            v-if="item.IsYear === '1'"
-            class="historyList-li history-red"
-          >
-            <span class="historyList-li_date">
-              {{ item.Date }}
-            </span>
-            {{ item.Detail }}
-          </li>
-          <li
-            v-else
-            class="historyList-li"
-          >
-            <span class="historyList-li_date">{{ item.Date }}</span>
-            {{ item.Detail }}
-          </li>
-        </span>
-      </ul>
-    </div>
+          <p>
+            {{ item.Date }}
+          </p>
+          <p>
+            <a
+              :href="item.Link || undefined"
+              :class="item.Link && 'text-primary hover:underline'"
+              target="_blank"
+            >{{ item.Detail }}</a>
+          </p>
+        </li>
+      </template>
+    </ul>
   </div>
 </template>
 
-<style lang="scss">
-.historyList {
-  margin-top: 4rem;
-
-  &-ul {
-    list-style: none;
-    margin-bottom: 2rem;
-  }
-
-  &-li {
-    font-weight: bold;
-    margin-bottom: 0.8rem;
-
-    &_date {
-      margin-right: 1rem;
-    }
-  }
-}
-
-.history-red {
-  margin-top: 3rem;
-  color: #ec3d3d;
-}
-
-@media screen and (max-width: 768px) {
-  .history-details_p {
-    margin-left: 5px;
-  }
-
-  .historyList-ul {
-    padding-left: 0.5rem;
-  }
-
-  .historyList-li {
-    margin-bottom: 30px;
-  }
-
-  .historyList-li_date {
-    display: block;
-  }
-}
-</style>
+<style></style>
