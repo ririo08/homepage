0e131ef9d770a32527c43fdc0b45742bd5a7e68d
ririo08 <7778943+ririo08@users.noreply.github.com>
2025-05-29 23:26:42 +0900

    feat: PC・周辺機器 置き換え

--- app/pages/pc-spec/index.vue
+++ app/pages/pc-spec/index.vue
@@ -1,46 +1,54 @@
-<script>
-export default {
-  data() {
-    return { list: [] }
-  },
-  async fetch() {
-    // const res = await this.$axios.$get('https://ririo.dev/db/pc-gadget.json')
-    // this.list = res
-  },
+<script setup lang="ts">
+type GadgetList = {
+  ProductName: string
+  BrandName: string
+  ImgLink: string
+  Link: string
+  IsCategory: string
+  PostedDate: string
+  Ranking: string
 }
+const { data: list } = await useFetch<GadgetList[]>('https://ririo.dev/db/pc-gadget.json')
 </script>
 
 <template>
   <div>
-    <div class="globalBox">
-      <h1 class="globalBox-title">
-        PC & Gear DETAIL
-      </h1>
-    </div>
+    <h2 class="text-2xl text-center font-bold border-b-2 border-primary-500 pb-2 mb-4">
+      PC & Gear DETAIL
+    </h2>
     <p>
-      旧使用機器は<NuxtLink to="/pcSpec/oldGear">
+      <span>旧使用機器は</span>
+      <NuxtLink
+        to="/pcSpec/oldGear"
+        class="text-info-500 hover:underline font-bold"
+      >
         こちら
       </NuxtLink>
     </p>
-    <section class="pcSpec-list">
-      <div class="pcSpec-description pcSpec-description_pc">
-        <h2>☆4代目Game&Stream PC</h2>
+    <section class="md:grid grid-cols-2 my-4">
+      <div>
+        <h2 class="font-bold text-lg mb-2">
+          ☆4代目Game&Stream PC
+        </h2>
         <p>CPU：Intel(R) Core(TM) i7-8700K CPU @ 3.70GHz</p>
         <p>GPU：NVIDIA GeForce RTX 2080</p>
         <p>RAM：32GB, CORSAIR DDR4 VENGEANCE LPX Series 16GB×2</p>
         <p>SSD：1TB</p>
-        <p class="pcSpec-detail">
-          詳細は<NuxtLink to="pcSpec/detail">
-            <strong>こちら</strong>
+        <p class="mt-4">
+          <span>詳細は</span>
+          <NuxtLink
+            to="pcSpec/detail"
+            class="text-info-500 hover:underline font-bold"
+          >
+            こちら
           </NuxtLink>
         </p>
         <p>
-          使用ソフト：<a
-            href="https://soundengine.jp/wordpress/penguin_press/press_release/3554/"
-          >WIN SOCRE SHARE</a>
+          <span>使用ソフト：</span>
+          <a href="https://soundengine.jp/wordpress/penguin_press/press_release/3554/">WIN SOCRE SHARE</a>
         </p>
       </div>
-      <div class="pcSpec-img">
+      <div>
         <img
           src="~assets/img/pcSpec/4thPC.png"
           alt="PC4世代目グラフ"
@@ -50,55 +58,56 @@ export default {
     <section
       v-for="item in list"
       :key="item.ProductName"
-      class="pcSpec-list"
+      class="mb-4"
     >
-      <div class="pcSpec-description">
-        <h2 v-if="item.IsCategory === 'TRUE'">
-          ☆{{ item.ProductName }}
-        </h2>
-        <div
-          v-else
-          class="amazlet-box"
-        >
-          <div class="amazlet-image">
+      <h2
+        v-if="item.IsCategory === 'TRUE'"
+        class="text-xl font-bold text-primary"
+      >
+        {{ item.ProductName }}
+      </h2>
+      <div
+        v-else
+        class="md:flex gap-4 pb-8"
+      >
+        <div class="aspect-square w-40 bg-white flex justify-center m-auto md:m-0">
+          <a
+            :href="item.Link"
+            target="_blank"
+            class="self-center"
+          >
+            <img
+              :src="item.ImgLink"
+              alt="商品画像"
+            >
+          </a>
+        </div>
+        <div class="my-2">
+          <div>
             <a
               :href="item.Link"
-              name="amazletlink"
               target="_blank"
+              class="text-lg font-bold text-primary-600 hover:underline hover:text-primary-700"
             >
-              <img
-                :src="item.ImgLink"
-                alt="商品画像"
-              >
+              {{ item.ProductName }}
             </a>
+            <div class="text-sm">
+              posted at {{ item.PostedDate }}
+            </div>
           </div>
-          <div class="amazlet-info">
-            <div class="amazlet-name">
+          <div>
+            <span>{{ item.BrandName }}</span><br>
+            <span>売り上げランキング: <span class="font-bold">{{ item.Ranking }}</span></span>
+          </div>
+          <div>
+            <div>
               <a
-                href="https://amzn.to/32coaxm"
-                name="amazletlink"
+                :href="item.Link"
                 target="_blank"
+                class="text-info-600"
               >
-                {{ item.ProductName }}
+                Amazon.co.jpで詳細を見る
               </a>
-              <div class="amazlet-powered-date">
-                posted at {{ item.PostedDate }}
-              </div>
-            </div>
-            <div class="amazlet-detail">
-              {{ item.BrandName }}<br>
-              売り上げランキング: {{ item.Ranking }}<br>
-            </div>
-            <div class="amazlet-sub-info">
-              <div class="amazlet-link">
-                <a
-                  :href="item.Link"
-                  name="amazletlink"
-                  target="_blank"
-                >
-                  Amazon.co.jpで詳細を見る
-                </a>
-              </div>
             </div>
           </div>
         </div>
@@ -107,120 +116,4 @@ export default {
   </div>
 </template>
 
-<style lang="scss">
-$themeColor: orange;
-.pcSpec {
-  &-list {
-    display: flex;
-    justify-content: space-between;
-  }
-
-  &-description {
-    padding-left: 10px;
-    padding-right: 10px;
-    margin-left: 0;
-
-    &_pc {
-      width: 50%;
-    }
-
-    h2 {
-      margin-top: 20px;
-      font-size: 16px;
-      font-weight: bold;
-      padding-bottom: 10px;
-    }
-
-    p {
-      font-size: 14px;
-      margin-bottom: 0.5rem;
-    }
-  }
-
-  &-img {
-    width: 50%;
-
-    img {
-      width: 100%;
-    }
-  }
-}
-
-/*! amazlet-css */
-$amazletLinks: #915500;
-
-.amazlet {
-  &-box {
-    display: flex;
-    justify-content: left;
-    margin-bottom: 50px;
-  }
-
-  &-image {
-    background-color: #fff;
-    display: flex;
-    width: 160px;
-    height: 160px;
-    & a {
-      display: block;
-      margin: auto;
-      & img {
-        display: block;
-        max-width: 100%;
-        margin: auto;
-      }
-    }
-  }
-
-  &-info {
-    padding-left: 20px;
-  }
-
-  &-name {
-    font-size: 16px;
-    a {
-      color: $amazletLinks;
-      font-weight: bold;
-    }
-  }
-
-  &-powered-date {
-    font-size: 12px;
-  }
-
-  &-detail {
-    font-size: 14px;
-  }
-
-  &-link {
-    a {
-      color: $amazletLinks;
-    }
-  }
-}
-
-@media screen and (max-width: 768px) {
-  .pcSpec-list {
-    display: block;
-    -webkit-box-pack: normal;
-    -ms-flex-pack: normal;
-    justify-content: normal;
-  }
-  .pcSpec-description {
-    width: 100%;
-  }
-  .pcSpec-img {
-    width: 100%;
-    margin-top: 30px;
-  }
-}
-
-@media screen and (max-width: 479px) {
-  .amazlet-box {
-    display: block;
-  }
-  .amazlet-info {
-    padding: 5px;
-  }
-}
-</style>
+<style></style>
