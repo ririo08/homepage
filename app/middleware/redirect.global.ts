be6feea9b65a7c565672c4aebff16e55688b21cc
ririo08 <7778943+ririo08@users.noreply.github.com>
2025-05-30 02:32:34 +0900

    chore: external navigateTo

--- app/middleware/redirect.global.ts
+++ app/middleware/redirect.global.ts
@@ -1,11 +1,11 @@
 export default defineNuxtRouteMiddleware((to) => {
   switch (to.path) {
     case '/pcSpec':
-      return navigateTo('/pc-spec', { redirectCode: 301 })
+      return navigateTo('/pc-spec', { redirectCode: 301, external: true })
     case '/pcSpec/oldGear':
-      return navigateTo('/pc-spec/old-gear', { redirectCode: 301 })
+      return navigateTo('/pc-spec/old-gear', { redirectCode: 301, external: true })
     case '/pcSpec/detail':
-      return navigateTo('/pc-spec/detail', { redirectCode: 301 })
+      return navigateTo('/pc-spec/detail', { redirectCode: 301, external: true })
     default:
       break
   }
