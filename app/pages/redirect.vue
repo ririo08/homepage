8840cb29b6df45f3048d7026d87bbb6a95c9f270
ririo08 <7778943+ririo08@users.noreply.github.com>
2025-05-31 02:04:33 +0900

    fix: リダイレクト処理をredirect.vueで行う

--- app/pages/redirect.vue
+++ app/pages/redirect.vue
@@ -10,7 +10,17 @@ onMounted(() => {
   const redirectPath = String(param).startsWith('/homepage')
     ? String(param).replace(/^\/homepage/, '') || '/'
     : String(param)
-  navigateTo(redirectPath)
+
+  switch (redirectPath) {
+    case '/pcSpec':
+      return navigateTo('/pc-spec', { redirectCode: 301 })
+    case '/pcSpec/oldGear':
+      return navigateTo('/pc-spec/old-gear', { redirectCode: 301 })
+    case '/pcSpec/detail':
+      return navigateTo('/pc-spec/detail', { redirectCode: 301 })
+    default:
+      return navigateTo('/', { redirectCode: 301 })
+  }
 })
 </script>
 
